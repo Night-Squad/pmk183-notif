99fb6dfbe5602ab6494079c45d9da0a907464238
Leni Salsabilaa <lenisalsabilaa@gmail.com>
2023-09-01 17:09:35 +0700

    update kafka v5

--- src/main/java/com/bjbs/haji/business/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/bjbs/haji/business/kafka/config/KafkaProducerConfig.java
@@ -17,27 +17,27 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaProducerConfig {
     
-     @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+    
+    public Map<String, Object> producerConfig(){
+    	 Map<String, Object> props = new HashMap<>();
+         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-    public Map<String, Object> producerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        return props;
+         return props;
     }
-
+    
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfig());
+    public ProducerFactory<String,String> producerFactory(){
+    	return new DefaultKafkaProducerFactory<>(producerConfig());
     }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(
-            ProducerFactory<String, String> producerFactory
-    ) {
-        return new KafkaTemplate<>(producerFactory);
-    }
+    
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate(
+//    		ProducerFactory<String, String> producerFactory
+//    		){
+//    	return new KafkaTemplate<>(producerFactory);
+//    } 
 }
