57dc307034532bf0f6b3e2f11cba32a56a6e3495
Diaz Pratama Putra <diazpratamaputra.work@gmail.com>
2023-09-04 16:58:11 +0700

    fixed topic partition and successfully get the same message at same time

--- src/main/java/com/bjbs/haji/business/kafka/component/KafkaListeners.java
+++ src/main/java/com/bjbs/haji/business/kafka/component/KafkaListeners.java
@@ -1,13 +1,17 @@
 package com.bjbs.haji.business.kafka.component;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
     @KafkaListener(
             topics = "setoran_awal_incoming",
-            groupId = "setoran-awal"
+            groupId = "setoran-awal",
+            topicPartitions = {
+                    @TopicPartition(topic = "setoran_awal_incoming", partitions = { "0", "1", "2" })
+            }
     )
     void listener(String data) {
         System.out.println("Listener received: " + data);
