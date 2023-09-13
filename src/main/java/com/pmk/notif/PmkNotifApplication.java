3709134d9d6fa76f3a1e20e0c6ae500d3765e908
wiyanayusuf <wiyana.yusuf@gmail.com>
2023-09-13 23:48:55 +0700

    [B001SP001] Removed eureka annotation

--- src/main/java/com/pmk/notif/PmkNotifApplication.java
+++ src/main/java/com/pmk/notif/PmkNotifApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication()
-//@EnableEurekaClient
 @Configuration
 public class PmkNotifApplication {
 
